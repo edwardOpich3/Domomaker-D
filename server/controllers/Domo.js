c1512a75edb0d66cdc19b25a3a7ad263686c3582
edwardOpich3 <edwardOpich3@gmail.com>
2018-10-24 17:41:42 -0400

    Fixed errors; Domomaker B

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,17 +1,17 @@
-const makerPage = (req, res) => {
-    Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
-        if(err){
-            console.log(err);
-            return res.status(400).json({ error: "An error occurred" });
-        }
-
-        return res.render("app", { domos: docs });
-    });
-};
-
 const models = require('../models');
 
 const Domo = models.Domo;
+
+const makerPage = (req, res) => {
+  Domo.DomoModel.findByOwner(req.session.account._id, (err, docs) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({ error: 'An error occurred' });
+    }
+
+    return res.render('app', { domos: docs });
+  });
+};
 
 const makeDomo = (req, res) => {
   if (!req.body.name || !req.body.age) {
